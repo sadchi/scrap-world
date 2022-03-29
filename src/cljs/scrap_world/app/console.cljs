5b5a547cec2da354df02fda81c33c9efb0170ec9
sadchi <sadchi@gmail.com>
2022-03-30 02:33:55 +0300

    WIP

--- src/cljs/scrap_world/app/console.cljs
+++ src/cljs/scrap_world/app/console.cljs
@@ -3,40 +3,28 @@
     [cljs-time.core :as t]
     [cljs-time.format :as tf]
     [garden.units :as g]
-    [reagent.core :as r]
-    [scrap-world.common.core :as c]
     [instaparse.core :as insta]
     [instaparse.failure :as fail]
+    [reagent.core :as r]
+    [reagent.dom :as rd]
     [scrap-world.common.common-styles :as cs]
+    [scrap-world.common.core :as c]
     [scrap-world.common.inputs :as i]
     ))
 
 (def params
-  {::log-depth     100
-   ::history-depth 30
-   ::logs-depth    30
-
-   ::background    "rgba(255,255,255,0.4);"})
+  {::log-depth         100
+   ::history-depth     30
+   ::logs-depth        30
+   ::timestamp-padding 170
+   ::generic-padding   16
+   ::background        "#9E9E9E"
+   ::colors            {::bad     "#B00020"
+                        ::good    "#004D40"
+                        ::neutral "#212121"}})
 
 (defn v [& k]
   (get-in params k))
-
-
-(def g' "
-res              = func | num | math-exp | math-op
-func             = func-name <'('> func-params <')'>
-func-name        = 'max' | 'min' | 'abs' | 'eq' | 'neq' | 'contains' | 'ncontains'
-func-params      = func-param | (func-param <' '*','' '*>)+ func-param
-func-param       = num | math-exp | math-op | func | str | var
-math-exp         = <'('> math-op <')'>
-math-op          = math-op-var math-sign math-op-var
-<math-op-var>    = num | var | func | math-exp | math-op
-math-sign        = <' '*> math-s <' '*>
-<math-s>         = '*' | '+' | '-' | '/'
-num              = #'[+-]?([0-9]*[.])?[0-9]+'
-str              = <'\"'> #'[^\"]*' <'\"'>
-var              = 'x'
-")
 
 (def g "
 res             = per-cmd-help | generic-help | cmd
@@ -144,13 +132,27 @@ str-val         = <'\"'> #'[^\"]*' <'\"'>
                                           :right      0
                                           :top        0
                                           :bottom     0
-                                          :overflow-y "scroll"})
+                                          :overflow-y "auto"})
 
 (def console-pane__logs__container__inner ^:css [cs/flex-box
                                                  {:flex-direction  "column"
                                                   :justify-content "flex-end"}])
 
 
+(def console-pane__logs__item ^:css {:margin-top    (g/px 8)
+                                     :margin-bottom (g/px 8)
+                                     :position      "relative"
+                                     :padding-left  (g/px (v ::timestamp-padding))
+                                     :padding-right (g/px (v ::generic-padding))})
+
+(def console-pane__logs__item--odd ^:css {:background "rgba(255, 255, 255, 0.2);"})
+
+(def console-pane__logs__item--bad ^:css {:color (v ::colors ::bad)})
+(def console-pane__logs__item--good ^:css {:color (v ::colors ::good)})
+(def console-pane__logs__item--neutral ^:css {:color (v ::colors ::neutral)})
+
+(def console-pane__logs__item__timestamp ^:css {:position "absolute"
+                                                :left     (g/px (v ::generic-padding))})
 
 (def console-pane__input ^:css {:flex-shrink   0
                                 :margin-bottom "8px"})
@@ -191,13 +193,27 @@ str-val         = <'\"'> #'[^\"]*' <'\"'>
       (c/log "run-cmd parsed-cmd: " cmd-parsed))))
 
 (defn console-logs []
-  [:div (c/cls 'console-pane__logs)
-   [:div (c/cls 'console-pane__logs__container)
-    [:div (c/cls 'console-pane__logs__container__inner)
-     (doall (for [item (reverse @log-stack)
-                  :let [{::keys [timestamp color content]} item]]
-              (for [[idx x] (map-indexed vector content)]
-                ^{:key idx} [:div x])))]]])
+  (let [node (atom nil)]
+    (r/create-class
+      {:component-did-mount  (fn [_]
+                               (reset! node (js/document.getElementById "logs")))
+       :component-did-update (fn [& _]
+                               (.scrollTo @node 0 (.-scrollHeight @node)))
+       :reagent-render       (fn []
+                               [:div (c/cls 'console-pane__logs)
+                                [:div#logs (c/cls 'console-pane__logs__container)
+                                 [:div (c/cls 'console-pane__logs__container__inner)
+                                  (doall (for [[idx item] (map-indexed vector (reverse @log-stack))
+                                               :let [{::keys [timestamp color content]} item]]
+                                           ^{:key idx} [:div (c/cls 'console-pane__logs__item
+                                                                    (when (odd? idx) 'console-pane__logs__item--odd)
+                                                                    (case color
+                                                                      :bad 'console-pane__logs__item--bad
+                                                                      :good 'console-pane__logs__item--good
+                                                                      :else 'console-pane__logs__item--neutral))
+                                                        [:div (c/cls 'console-pane__logs__item__timestamp) timestamp]
+                                                        (for [[i x] (map-indexed vector content)]
+                                                          ^{:key i} [:div x])]))]]])})))
 
 (defn console []
   [:div (c/cls 'console-pane
