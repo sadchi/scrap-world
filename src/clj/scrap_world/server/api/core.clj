facce3b0e34005662113ee0688feaf2f2f553fcc
sadchi <sadchi@gmail.com>
2022-04-10 12:22:45 +0300

    WIP

--- src/clj/scrap_world/server/api/core.clj
+++ src/clj/scrap_world/server/api/core.clj
@@ -4,18 +4,10 @@
     [clojure.string :as s]
     [clojure.tools.logging :as log]
     [hiccup.page :as h]
-    [malli.util :as mu]
-    [muuntaja.core :as m]
     [pandect.algo.md5 :refer [md5]]
-    [reitit.coercion.malli]
     [reitit.ring :as ring]
-    [reitit.ring.coercion :as coercion]
     [reitit.ring.malli]
-    [reitit.ring.middleware.exception :as exception]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.ring.middleware.parameters :as parameters]
     [reitit.swagger :as swagger]
-    [reitit.swagger-ui :as swagger-ui]
     ))
 
 (defn generate-resource-name [initial-name & {:keys [resource-prefix root skip-cache]}]
@@ -31,7 +23,7 @@
       (str resource-prefix "/app_" hash-str ".js"))))
 
 (defn get-js-resource [req]
-  (log/debug "Trying to get resources " req)
+  ;(log/debug "Trying to get resources " req)
   (let [
         res (get-in req [:path-params :res])
         res-processed (s/replace res #"_[\d\w]+.js" ".js")
@@ -43,63 +35,35 @@
                   (io/resource)
                   (io/input-stream))}))
 
-(defn all-routes [cfg]
-  (ring/ring-handler
-    (ring/router [
-                  ["/"
-                   {:get {:no-doc  true
-                          :handler (fn [_]
-                                     {:status  200
-                                      :headers {"Content-type" "text/html"}
-                                      :body    (h/html5
-                                                 [:head [:title "Scrap world"]
-                                                  [:meta {:name    "front-params"
-                                                          :content (prn-str (:front cfg))}]]
-                                                 [:body
-                                                  [:div#dimmer]
-                                                  [:div#alerter]
-                                                  [:div#tooltip]
-                                                  [:div#modal]
-                                                  [:div#app]
-                                                  (h/include-js (generate-resource-name "app.js"
-                                                                                        :skip-cache (:skip-resource-cache cfg)
-                                                                                        :resource-prefix "/js"))])})}}]
-                  ["/js/:res" {:no-doc  true
-                               :handler (fn [req]
-                                          (get-js-resource req))}]
-                  ["/public/*" (ring/create-resource-handler)]
-                  ["/swagger.json"
-                   {:get {:no-doc  true
-                          :swagger {:info {:title       "Scrap-world api"
-                                           :description "Some description"}
-                                    ;:tags [{:name "files", :description "file api"}
-                                    ;       {:name "math", :description "math api"}]
-                                    ;
-                                    }
-                          :handler (swagger/create-swagger-handler)}}]]
-                 {
-                  :data {:coercion   (reitit.coercion.malli/create
-                                       {
-                                        :error-keys       #{#_:type :coercion :in :schema :value :errors :humanized #_:transformed}
-                                        :compile          mu/open-schema
-                                        :strip-extra-keys true
-                                        :default-values   true
-                                        :options          nil})
-                         :muuntaja   m/instance
-                         :middleware [
-                                      swagger/swagger-feature
-                                      parameters/parameters-middleware
-                                      muuntaja/format-negotiate-middleware
-                                      muuntaja/format-response-middleware
-                                      exception/exception-middleware
-                                      muuntaja/format-request-middleware
-                                      coercion/coerce-response-middleware
-                                      coercion/coerce-request-middleware
-                                      ]}})
-    (ring/routes
-      (swagger-ui/create-swagger-ui-handler
-        {:path   "/swagger"
-         :config {:validatorUrl     nil
-                  :operationsSorter "alpha"}})
-      (ring/create-default-handler))))
+(defn main [cfg]
+  [["/" {:get {:no-doc  true
+               :handler (fn [_]
+                          {:status  200
+                           :headers {"Content-type" "text/html"}
+                           :body    (h/html5
+                                      [:head [:title "Scrap world"]
+                                       [:meta {:name    "front-params"
+                                               :content (prn-str (:front cfg))}]]
+                                      [:body
+                                       [:div#dimmer]
+                                       [:div#alerter]
+                                       [:div#tooltip]
+                                       [:div#modal]
+                                       [:div#app]
+                                       (h/include-js (generate-resource-name "app.js"
+                                                                             :skip-cache (:skip-resource-cache cfg)
+                                                                             :resource-prefix "/js"))])})}}]
+   ["/js/:res" {:no-doc  true
+                :handler (fn [req]
+                           (get-js-resource req))}]
+   ["/public/*" {:no-doc  true
+                 :handler (ring/create-resource-handler)}]
+   ["/swagger.json" {:get {:no-doc  true
+                           :swagger {:info {:title       "Scrap-world api"
+                                            :description "Some description"}
+                                     ;:tags [{:name "files", :description "file api"}
+                                     ;       {:name "math", :description "math api"}]
+                                     ;
+                                     }
+                           :handler (swagger/create-swagger-handler)}}]])
 
--- src/clj/scrap_world/server/core.clj
+++ src/clj/scrap_world/server/core.clj
@@ -1,11 +1,49 @@
 (ns scrap-world.server.core
   (:require
+    [malli.util :as mu]
+    [muuntaja.core :as m]
     [org.httpkit.server :refer [run-server]]
+    [reitit.coercion.malli :as rm]
+    [reitit.ring :as ring]
+    [reitit.ring.coercion :as coercion]
+    [reitit.ring.malli]
+    [reitit.ring.middleware.exception :as exception]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.parameters :as parameters]
+    [reitit.swagger :as swagger]
+    [reitit.swagger-ui :as swagger-ui]
     [scrap-world.server.api.core :as api]
     ))
 
 
+(defn app [cfg]
+  (ring/ring-handler
+    (ring/router
+      [(api/main cfg)]
+      {:data {:coercion   (rm/create {
+                                      :error-keys       #{#_:type :coercion :in :schema :value :errors :humanized #_:transformed}
+                                      :compile          mu/open-schema
+                                      :strip-extra-keys true
+                                      :default-values   true
+                                      :options          nil})
+              :muuntaja   m/instance
+              :middleware [swagger/swagger-feature
+                           parameters/parameters-middleware
+                           muuntaja/format-negotiate-middleware
+                           muuntaja/format-response-middleware
+                           exception/exception-middleware
+                           muuntaja/format-request-middleware
+                           coercion/coerce-response-middleware
+                           coercion/coerce-request-middleware]}})
+    (ring/routes
+      (swagger-ui/create-swagger-ui-handler
+        {:path   "/swagger"
+         :config {:validatorUrl     nil
+                  :operationsSorter "alpha"}})
+      (ring/create-default-handler))))
+
+
 (defn start [cfg]
-  (run-server (api/all-routes cfg) (:server cfg)))
+  (run-server (app cfg) (:server cfg)))
 
 
